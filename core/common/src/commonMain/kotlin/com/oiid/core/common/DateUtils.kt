8ce63828d642669d6fe747547b19f6b4170635f4
Sam <sam.henwood@gmail.com>
2025-09-03 17:39:46 +0900

    Tweak: more relative date granularity (years/months/weeks)

--- core/common/src/commonMain/kotlin/com/oiid/core/common/DateUtils.kt
+++ core/common/src/commonMain/kotlin/com/oiid/core/common/DateUtils.kt
@@ -21,11 +21,19 @@ fun formatRelativeTime(pastMillis: Instant, nowMillis: Long = Clock.System.now()
     val minutes = seconds / 60
     val hours = minutes / 60
     val days = hours / 24
+    val months = (days / 30)
+    val years = (days / 365)
 
     return when {
         seconds < 60 -> "just now"
         minutes < 60 -> "$minutes minute${if (minutes != 1L) "s" else ""} ago"
         hours < 24 -> "$hours hour${if (hours != 1L) "s" else ""} ago"
-        else -> "$days day${if (days != 1L) "s" else ""} ago"
+        months < 1 -> { // If less than a month but more than 7 days, consider showing weeks
+            val weeks = days / 7
+            if (weeks > 0) "$weeks week${if (weeks != 1L) "s" else ""} ago"
+            else "$days day${if (days != 1L) "s" else ""} ago" // Fallback if less than a week
+        }
+        months < 12 -> "$months month${if (months != 1L) "s" else ""} ago"
+        else -> "$years year${if (years != 1L) "s" else ""} ago"
     }
 }
